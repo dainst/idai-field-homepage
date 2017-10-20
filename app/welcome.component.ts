917968e06547cbc2d60ad15b3de6039aa3ceab80
phg <philipp.gerth@gmx.net>
2017-10-20 11:25:46 +0200

    LandingPage basic implementation of project map

--- app/welcome.component.ts
+++ app/welcome.component.ts
@@ -14,6 +14,8 @@ import {Document} from 'idai-components-2/core';
 export class WelcomeComponent implements OnInit {
 
     private documents: Array<Document>;
+    public map: L.Map;
+    public baseMaps: any;
 
     constructor(private datastore: ReadDatastore,
                 private router: Router
@@ -21,6 +23,32 @@ export class WelcomeComponent implements OnInit {
 
     ngOnInit() {
 
+        this.baseMaps = {
+            OpenStreetMap: L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a></a>'
+            }),
+            RomanEmpire: L.tileLayer("http://dare.ht.lu.se/tiles/imperium/{z}/{x}/{y}.png", {
+                attribution: '&copy; <a href="http://dare.ht.lu.se/">Digital Atlas of the Roman Empire</a></a>'
+            }),
+            CartoDB: L.tileLayer("http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png", {
+                attribution: '&copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+            })
+        };
+
+        // initialize map & mapping parameters
+        this.map = L.map('projectMap', {
+            center: new L.LatLng(40, 15),
+            zoomControl: false,
+            zoom: 5,
+            layers: [this.baseMaps.CartoDB],
+        });
+
+        // adds map control: layertree, scale
+        L.control.zoom({position: 'topleft'}).addTo(this.map);
+        L.control.layers(this.baseMaps).addTo(this.map);
+        L.control.scale().addTo(this.map);
+
+        // add project data
         this.datastore.find({ q: '', type: 'Project' }).then(
             documents => {this.documents = documents; console.log(documents)},
             err => console.error(err)
@@ -29,7 +57,14 @@ export class WelcomeComponent implements OnInit {
 
     public showDocument(document: Document) {
         this.router.navigate(['resources/show/' + document.resource.id]);
-        
+
+        /*
+        let popup = L.popup()
+            .setLatLng(document.resource.geometry.coordinates[0], document.resource.geometry.coordinates[1])
+            .setContent('<p>TITLE<br />' + document.resource.type)
+            .openOn(this.map);
+        */
+        // alert(document.resource.geometry.coordinates[0]);
     }
 
 
