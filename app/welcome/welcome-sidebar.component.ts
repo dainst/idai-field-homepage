71bd9f31a7ae0b677d6e3432da68d7d0baaf3946
phg <philipp.gerth@gmx.net>
2017-12-04 10:46:31 +0100

    Deactivate sidebar links, if no properties available

--- app/welcome/welcome-sidebar.component.ts
+++ app/welcome/welcome-sidebar.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
     moduleId: module.id,
     selector: 'welcome-sidebar',
-    templateUrl: './welcome-sidebar.html'
+    templateUrl: './welcome-sidebar.html',
 })
 /**
  * @author Philipp Gerth
@@ -13,4 +14,10 @@ export class WelcomeSidebarComponent {
 
     @Input() document: any;
 
+    constructor(private sanitization:DomSanitizer) {
+    }
+
+    private changeColor() {
+        return this.sanitization.bypassSecurityTrustStyle('color:#2A2B2C');
+    }
 }
--- app/welcome/welcome.component.ts
+++ app/welcome/welcome.component.ts
@@ -81,8 +81,6 @@ export class WelcomeComponent implements OnInit {
 
     private selectDocumentOnSidebar(doc: Document) {
 
-        console.log(doc);
-
         this.selectedDocument = doc.resource;
         this.selectedDocument.name = TitleGenerator.resolveProjectName(doc.resource.identifier);
     }
