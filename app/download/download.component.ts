42b2567603f28640d19d538f4495abe8efbe210c
tkleinke <thomas.kleinke@dainst.de>
2021-04-23 14:23:22 +0200

    Fix image URLs

--- app/download/download.component.ts
+++ app/download/download.component.ts
@@ -18,27 +18,27 @@ export class DownloadComponent implements OnInit {
 
     private images = [
         {
-            url: "https://raw.githubusercontent.com/dainst/idai-field/master/img/README-FEATURES-1.png",
+            url: "https://raw.githubusercontent.com/dainst/idai-field/master/desktop/img/README-FEATURES-1.png",
             desc: "Metadata editor"
         },
         {
-            url: "https://raw.githubusercontent.com/dainst/idai-field/master/img/README-FEATURES-2.png",
+            url: "https://raw.githubusercontent.com/dainst/idai-field/master/desktop/img/README-FEATURES-2.png",
             desc: "Shape editor"
         },
         {
-            url: "https://raw.githubusercontent.com/dainst/idai-field/master/img/README-FEATURES-8.png",
+            url: "https://raw.githubusercontent.com/dainst/idai-field/master/desktop/img/README-FEATURES-8.png",
             desc: "Matrix view"
         },
         {
-            url: "https://raw.githubusercontent.com/dainst/idai-field/master/img/README-FEATURES-6.png",
+            url: "https://raw.githubusercontent.com/dainst/idai-field/master/desktop/img/README-FEATURES-6.png",
             desc: "Synchronization"
         },
         {
-            url: "https://raw.githubusercontent.com/dainst/idai-field/master/img/README-FEATURES-3.png",
+            url: "https://raw.githubusercontent.com/dainst/idai-field/master/desktop/img/README-FEATURES-3.png",
             desc: "Table view"
         },
         {
-            url: "https://raw.githubusercontent.com/dainst/idai-field/master/img/README-FEATURES-4.png",
+            url: "https://raw.githubusercontent.com/dainst/idai-field/master/desktop/img/README-FEATURES-4.png",
             desc: "Nesting"
         }
     ];
